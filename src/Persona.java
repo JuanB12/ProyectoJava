df1baaf8aecee0c8e3c367427ce6d0bcc22cd9cd
Juan Barrios <jcamilobb03@gmail.com>
2022-03-23 09:03:09 -0500

    Se crea la clase Persona

---
+++ src/Persona.java
@@ -0,0 +1,54 @@
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Persona {
+
+    private String Nombre;
+    private String Apellido;
+    private long Cedula;
+    private long Telefono;
+    private long Direccion;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public long getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(long Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public long getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(long Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public long getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(long Direccion) {
+        this.Direccion = Direccion;
+    }
+
+}
