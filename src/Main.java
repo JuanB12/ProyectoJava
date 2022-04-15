b91689efce3a265f8169e1b4938e3fd5da77f18f
Nombre completo <jcamilobb03.com>
2022-04-14 22:15:54 -0500

    Adelanto de la interfaz grafica de login

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+        Factura factura = new Factura();
+        Inventario inventario = new Inventario();
+        Persona persona = new Persona();
+        Producto producto = new Producto();
+        Proovedor proovedor = new Proovedor();
+        Recibo recibo = new Recibo();
+        Vendedor vendedor = new Vendedor();
+        
+    }
+}
